c1d849d88cd0446b68a9b3990327bf380256080a
phucbm <hiphucbui@gmail.com>
2021-06-26 09:48:11 +0700

    following button using gsap

--- magnetic-button/script.js
+++ magnetic-button/script.js
@@ -1,27 +1,29 @@
 $(document).ready(function () {
     let $area = $(".area-wrap"),
         $button = $area.find("a.btn"),
-        limit_x_negative = ($button.width() - $area.width())/2,
+        limit_x_negative = ($button.width() - $area.width()) / 2,
         limit_x_positive = -1 * limit_x_negative,
-        limit_y_negative = ($button.height() - $area.height())/2,
+        limit_y_negative = ($button.height() - $area.height()) / 2,
         limit_y_positive = -1 * limit_y_negative;
 
 
-    $area.on("mousemove", function(e) {
-        let x = e.pageX - $(this).position().left + limit_x_negative - $button.width()/2,
-            y = e.pageY - $(this).position().top + limit_y_negative- $button.height()/2;
+    $area.on("mousemove", function (e) {
+        let x = e.pageX - $(this).position().left + limit_x_negative - $button.width() / 2,
+            y = e.pageY - $(this).position().top + limit_y_negative - $button.height() / 2;
 
         console.log("X:" + x + "px");
         console.log("Y:" + y + "px");
 
-        $button.css({
-            "transform":"translate(" + x + "px," + y + "px)",
-        })
+        /*$button.css({
+            "transform": "translate(" + x + "px," + y + "px)",
+        })*/
+        gsap.to($button, {duration: .5, x: x, y: y});
     })
 
-    $area.on("mouseout", function(e) {
-        $button.css({
-            "transform":"translate(0px,0px)",
-        })
+    $area.on("mouseout", function (e) {
+        /*$button.css({
+            "transform": "translate(0px,0px)",
+        })*/
+        gsap.to($button, {duration: .5, x: 0, y: 0});
     })
 })
